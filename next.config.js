a6e5c3e9291794d9400933807ce03c7a47b1eb8f
wangu567 <138377636+wangu567@users.noreply.github.com>
2025-09-25 11:27:36 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// next.config.js
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Make "@/..." work at build time
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
